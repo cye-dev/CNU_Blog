c2bf94ac4481f8ab9d3217f6c05db55e5afe71d2
YE <uniye21@gmail.com>
2023-06-06 11:16:36 +0900

    fix Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,17 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <h2>실전코딩 수강</h2>
+      <p>2023.03 ~ 2023.06</p>
+      <p>Github, SpringBoot, React 등 실무 관련 지식 수강</p>
+      <h2>학부 연구생</h2>
+      <p>2022.11 ~</p>
+      <p>분산 네트워크 및 컴퓨팅 연구실에서 학부 연구생으로 참여, AI 분야 연구에 참여중</p>
+      <h2>2022 CNU Project Fair 주니어 창의작품경진대회 대상 수상</h2>
+      <p>2022.06</p>
+      <p>식물 질병 분류 모델을 이용한 질의응답 서비스 앱 개발</p>
+    </div>
+  );
 };
 
 export default Resume;
